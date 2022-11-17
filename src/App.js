3449eca4f13b9489a4901b70a9d3fcd8cab64ecb
Anton Durandin <enitefall81@gmail.com>
2022-11-17 14:59:06 -0800

    Refactor and delete buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react"
-import DisplayCertainCategory from "./DisplayCertainCategory"
+import Categories from "./Categories"
+import CategoryItem from "./Categories"
 
-let categories = [
+let initialCategories = [
   {selected: false, text: "First Category" , id: 1},
   {selected: false, text: "Second Category", id: 2},
   {selected: false, text: "Third Category", id: 3},
@@ -9,28 +10,18 @@ let categories = [
   {selected: false, text: "No category", id: 5}
 ]
 
-const listOfCategories = categories.map((element) => {
-  return <li key={element.id}>
-    {element.text}
-         <button type="button" onClick={_ => removeCategory(listOfCategories.text)}
-//у каждой категории кнопка удаление, непонятно через список делать и вообще
-// прямо тут или в DisplayCertainCategory компоненте
 
-                  className="btn btn-secondary ml-2"
-                  style={{lineHeight: 1, padding: "0.125rem .25rem"}}>
-       </button>
-  </li>
-})
 
-function removeCategory(text) {
-  return 0
-}
+
 
 function App() {
-  //тут стейт сделаем для категорий
-  //let [categories, setCategories] = useState(categories)
+  function removeCategory(text) {
+  return 0
+}
+  //тут стейт сделаем для категорий, но почему-то cannot access before init.
+  let [categories, setCategories] = useState(initialCategories)
   return <div>
-      <DisplayCertainCategory text={listOfCategories} removeCategory={removeCategory}></DisplayCertainCategory>
+      <Categories categories={categories} removeCategory={removeCategory}></Categories>
       </div>
 }
 
---
+++ src/Categories.js
@@ -0,0 +1,35 @@
+
+import React, {useState} from "react"
+
+
+
+
+
+
+function Categories({categories, removeCategory}) {
+
+
+  return <div>
+   <ul style={{listStyleType: "none", paddingLeft: 0}}>
+     {categories.map(category =>
+        <CategoryItem category={category} removeCategory={removeCategory}/>
+      )
+     }
+   </ul>
+  </div>
+}
+function CategoryItem({category, removeCategory}) {
+  return <div>
+    <li>
+      {console.log(category)}
+      {category.text}
+       <button type="button" onClick={_ => removeCategory(category.text)}
+                  className="btn btn-secondary ml-2"
+                  style={{lineHeight: 1, padding: "0.125rem .25rem"}}>
+            &times;
+          </button>
+    </li>
+  </div>
+}
+
+export default Categories
