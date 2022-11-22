2b09a5e328bfdbbbb811f8340faef3824265ef58
Anton Durandin <enitefall81@gmail.com>
2022-11-21 16:17:30 -0800

    Formatting fixed

--- src/Item.js
+++ src/Item.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 function Item({ item }) {
-   return (
+  return (
     <div>
       <table>
-          <thead>
-            <tr>
-              <th>{item.name}</th>
-              <th>{item.purchasePrice}</th>
-              <th>{item.salePrice}</th>
-            </tr>
-          </thead>
-</table>
+        <thead>
+          <tr>
+            <th>{item.name}</th>
+            <th>{item.purchasePrice}</th>
+            <th>{item.salePrice}</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
 
-export default Item
+export default Item;
