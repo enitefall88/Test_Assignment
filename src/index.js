1e6faa4fed3194c3d753af0b0c46fbb664a401a9
enitefall <enitefall81@gmail.com>
2024-05-27 20:32:42 -0700

    for github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import App from './Components/App';
+import App from './Components/App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
